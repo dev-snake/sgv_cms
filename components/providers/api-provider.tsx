e2d95f70b4473bb3e742ca85bced31e527755385
dev_snake <dangvanhaufpt2019@gmail.com>
2026-02-05 10:31:59 +0700

    refactor: update styles and improve responsiveness in ChatWidget and AreaChartGradient components

--- components/providers/api-provider.tsx
+++ components/providers/api-provider.tsx
@@ -21,10 +21,8 @@ let browserQueryClient: QueryClient | undefined = undefined;
 
 function getQueryClient() {
     if (typeof window === 'undefined') {
-        // Server: always make a new query client
         return makeQueryClient();
     } else {
-        // Browser: make a new query client if we don't already have one
         if (!browserQueryClient) browserQueryClient = makeQueryClient();
         return browserQueryClient;
     }
